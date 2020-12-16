4a79bfdbc46187195368401b8c9b32e4e8f94a84
Aurore-dev <mounier.aurore@gmail.com>
2020-12-16 13:59:03 +0100

    Déclaration d'une fonction

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-alert('le fichier est bien en place');
+function initialisationJS(){
+    var jsload = document.querySelector('#jsload');
+    jsload.innerHTML='mon <span style="font-weight:900">JS</span> est chargé !';
+    jsload.style.backgroundColor='LIME';
+}
+
+initialisationJS();
