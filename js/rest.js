f6147bea97d8f67271d1e0aff61db04846c8c80a
Aurore-dev <mounier.aurore@gmail.com>
2020-12-17 14:20:31 +0100

    Mise en place d'une fonction POST

--- js/rest.js
+++ js/rest.js
@@ -1,12 +1,14 @@
+var BASE_URL='http://localhost:2607';
+
 /**
  * Permet l'apple HTTP avec XMLHttpRequest
  * @param {*} ressourceUrl chemin de la reponse
  */
 function get (ressourceUrl){
     //instanciation de XHR
-    new xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
     //ouverture de la connexion
-    xhr.open('GET','http://localhost:2607'+ressourceUrl);
+    xhr.open('GET',BASE_URL+ressourceUrl);
     //tache à efectuer à chaque changement de readystate(passage d'une étape de réception)
     //1 open   2 send       3 encours de reception    4 fin de reception
     xhr.onreadystatechange=function(evt){
@@ -17,4 +19,25 @@ function get (ressourceUrl){
     //envoi de la requete
     xhr.send();
 }
-get('/postit/1');
+
+/**
+ * Permet l'envoi en post d'une ressource sur l'a ressourceUrl
+ * @param {Uri} ressourceUrl chemin du post
+ * @param {Object} ressource  data à envoyer
+ */
+function post(ressourceUrl,ressource){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST',BASE_URL+ressourceUrl);
+    //specfication du type de contenu
+    xhr.setRequestHeader('Content-Type','application/json');
+    //specification de ce qui est eattendu en retour
+    xhr.setRequestHeader('Accept','application/json');
+    xhr.onreadystatechange=function(evt){
+        if(xhr.readyState < 4){return;}
+        console.log(JSON.parse(xhr.response));
+    }
+    //transformation en JSON des données
+    xhr.send(JSON.stringify(ressource));
+}
+
+get('/postit');
